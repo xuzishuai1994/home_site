eff710100d5b7b5189a94b39919cf0e9c316fce7
zishui.xu <zishui.xu@external.99bill.com>
2018-04-19 15:15:09 +0800

    <010>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import curvejs from 'curvejs';
-import { handleDebounce, handleDebounceDecorator } from './util';
+import { handleDebounce, handleDebounceDecorator } from './util/js';
 
 import s from './app.less';
 
